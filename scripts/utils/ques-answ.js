ef302fc36ec265a8dde02c17404580e148563b36
Vismyfriend <vismyfriend@yahoo.com>
2023-04-28 10:58:15 +0300

    addded answers

--- scripts/utils/ques-answ.js
+++ scripts/utils/ques-answ.js
@@ -18,6 +18,41 @@ const questions = [
         id: 4,
         ru: "читать книги",
         eng: "read books"
+    },
+    {
+        id: 5,
+        ru: "	плакать	",
+        eng: "	cry	"
+    },
+    {
+        id: 6,
+        ru: "	мыть полы	",
+        eng: "	wash the floors	"
+    },
+    {
+        id: 7,
+        ru: "	мыть посуду	",
+        eng: "	wash up = wash the dishes	"
+    },
+    {
+        id: 8,
+        ru: "	тратить деньги	",
+        eng: "	spend money	"
+    },
+    {
+        id: 9,
+        ru: "	покупать еду	",
+        eng: "	buy food	"
+    },
+    {
+        id: 10,
+        ru: "	пить текилу	",
+        eng: "	drink tequilla	"
+    },
+    {
+        id: 11,
+        ru: "	курить сигары	",
+        eng: "	smoke cigars	"
     }
 ]
 const answers = [
@@ -37,8 +72,43 @@ const answers = [
         eng: "before lessons"
     },
     {
+        id: 105,
+        ru: "	по понедельникам	",
+        eng: "	on Mondays	"
+    },
+    {
         id: 104,
         ru: "когда мне скучно",
         eng: "when I'm bored"
+    },
+    {
+        id: 106,
+        ru: "	по вторникам	",
+        eng: "	on Tuesdays	"
+    },
+    {
+        id: 107,
+        ru: "	по средам	",
+        eng: "	on Wednesdays	"
+    },
+    {
+        id: 108,
+        ru: "	 по четвергам	",
+        eng: "	on Thursdays	"
+    },
+    {
+        id: 109,
+        ru: "	по пятницам	",
+        eng: "	on Fridays	"
+    },
+    {
+        id: 110,
+        ru: "	по субботам	",
+        eng: "	on Saturdays	"
+    },
+    {
+        id: 111,
+        ru: "	по воскресеньям	",
+        eng: "	on Sundays	"
     }
-]
+]	
