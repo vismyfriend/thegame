5f5619801fd66b74d29d2428f7a4a8525756dcb9
Vismyfriend <vismyfriend@yahoo.com>
2023-05-02 09:57:15 +0300

    who and what

--- scripts/utils/ques-answ.js
+++ scripts/utils/ques-answ.js
@@ -1,124 +1,144 @@
 const questions = [
-    {
-        id: 1,
-        ru: "	Январь	",
-        eng: "	January	"
-    },
-    {
-        id: 2,
-        ru: "	Февраль	",
-        eng: "	February	"
-    },
-    {
-        id: 3,
-        ru: "	Март	",
-        eng: "	March	"
-    },
-    {
-        id: 4,
-        ru: "	Апрель	",
-        eng: "	April	"
-    },
-    {
-        id: 5,
-        ru: "	Май	",
-        eng: "	May	"
-    },
-    {
-        id: 6,
-        ru: "	Июнь	",
-        eng: "	June	"
-    },
-    {
-        id: 7,
-        ru: "	Июль	",
-        eng: "	July	"
-    },
-    {
-        id: 8,
-        ru: "	Август	",
-        eng: "	August	"
-    },
-    {
-        id: 9,
-        ru: "	Сентябрь	",
-        eng: "	September	"
-    },
-    {
-        id: 10,
-        ru: "	Октябрь	",
-        eng: "	October	"
-    },
-    {
-        id: 11,
-        ru: "	Ноябрь	",
-        eng: "	November	"
-    },
-    {
-        id: 12,
-        ru: "	Декабрь	",
-        eng: "	December	"
-    }
+    {					
+        id	:	1	,		
+        ru	:	"	пить пиво	"	,
+        eng	:	"	drink beer	"	
+        }	,				
+        {					
+        id	:	2	,		
+        ru	:	"	мыть полы	"	,
+        eng	:	"	wash the floor	"	
+        }	,				
+        {					
+        id	:	3	,		
+        ru	:	"	готовить еду	"	,
+        eng	:	"	cook food	"	
+        }	,				
+        {					
+        id	:	4	,		
+        ru	:	"	целовать иностранца	"	,
+        eng	:	"	kiss a foreigner	"	
+        }	,				
+        {					
+        id	:	5	,		
+        ru	:	"	купить ноутбук	"	,
+        eng	:	"	buy a laptop	"	
+        }	,				
+        {					
+        id	:	6	,		
+        ru	:	"	петь в душе	"	,
+        eng	:	"	sing in the shower	"	
+        }	,				
+        {					
+        id	:	7	,		
+        ru	:	"	делать домашку	"	,
+        eng	:	"	do homework	"	
+        }	,				
+        {					
+        id	:	8	,		
+        ru	:	"	спать	"	,
+        eng	:	"	sleep	"	
+        }	,				
+        {					
+        id	:	9	,		
+        ru	:	"	танцевать на столе	"	,
+        eng	:	"	dance on the table	"	
+        }	,				
+        {					
+        id	:	10	,		
+        ru	:	"	курить	"	,
+        eng	:	"	smoke	"	
+        }	,				
+        {					
+        id	:	11	,		
+        ru	:	"	усердно работать	"	,
+        eng	:	"	work hard	"	
+        }	,				
+        {					
+        id	:	12	,		
+        ru	:	"	отдыхать	"	,
+        eng	:	"	relax	"	
+        }	,				
+        {					
+        id	:	13	,		
+        ru	:	"	смотреть телефвизор	"	,
+        eng	:	"	watch TV	"	
+        }	,				
+        {					
+        id	:	14	,		
+        ru	:	"	играть в игры	"	,
+        eng	:	"	play video games	"	
+        }					
 ]
 const answers = [
-    {
-        id: 101,
-        ru: "	сто	",
-        eng: "	a hundred	"
-    },
-    {
-        id: 102,
-        ru: "	тысяча	",
-        eng: "	a thousand	"
-    },
-    {
-        id: 103,
-        ru: "	около миллиона	",
-        eng: "	about a million	"
-    },
-    {
-        id: 104,
-        ru: "	миллиард	",
-        eng: "	a billion	"
-    },
-    {
-        id: 105,
-        ru: "	сто один	",
-        eng: "	a hundred one	"
-    },
-    {
-        id: 106,
-        ru: "	сто десять	",
-        eng: "	a hundred ten	"
-    },
-    {
-        id: 107,
-        ru: "	тринадцать	",
-        eng: "	thirteen	"
-    },
-    {
-        id: 108,
-        ru: "	тридцать	",
-        eng: "	thirty	"
-    },
-    {
-        id: 109,
-        ru: "	двенадцать	",
-        eng: "	twelve	"
-    },
-    {
-        id: 110,
-        ru: "	двадцать	",
-        eng: "	twenty	"
-    },
-    {
-        id: 111,
-        ru: "	восемь с половиной	",
-        eng: "	eight and a half	"
-    },
-    {
-        id: 112,
-        ru: "	тридцать три	",
-        eng: "	thirty three	"
-    }
+    {					
+        id	:	101	,		
+        ru	:	"	я	"	,
+        eng	:	"	I 	"	
+        }	,				
+        {					
+        id	:	102	,		
+        ru	:	"	мой учитель	"	,
+        eng	:	"	My teacher	"	
+        }	,				
+        {					
+        id	:	103	,		
+        ru	:	"	мой ребенок	"	,
+        eng	:	"	my child	"	
+        }	,				
+        {					
+        id	:	104	,		
+        ru	:	"	мои соседи	"	,
+        eng	:	"	my neighbors	"	
+        }	,				
+        {					
+        id	:	105	,		
+        ru	:	"	Винсент	"	,
+        eng	:	"	Vincent	"	
+        }	,				
+        {					
+        id	:	106	,		
+        ru	:	"	Эминем	"	,
+        eng	:	"	Eminem	"	
+        }	,				
+        {					
+        id	:	107	,		
+        ru	:	"	Президент	"	,
+        eng	:	"	The president	"	
+        }	,				
+        {					
+        id	:	108	,		
+        ru	:	"	Полицейский	"	,
+        eng	:	"	a police officer	"	
+        }	,				
+        {					
+        id	:	109	,		
+        ru	:	"	Пожарник	"	,
+        eng	:	"	a fireman	"	
+        }	,				
+        {					
+        id	:	110	,		
+        ru	:	"	Повар	"	,
+        eng	:	"	a cook	"	
+        }	,				
+        {					
+        id	:	111	,		
+        ru	:	"	Чья-то собака	"	,
+        eng	:	"	someone's dog	"	
+        }	,				
+        {					
+        id	:	112	,		
+        ru	:	"	Чья-то кошка	"	,
+        eng	:	"	someone's cat	"	
+        }	,				
+        {					
+        id	:	113	,		
+        ru	:	"	Мы	"	,
+        eng	:	"	We	"	
+        }	,				
+        {					
+        id	:	114	,		
+        ru	:	"	Наши друзья	"	,
+        eng	:	"	Our friends	"	
+        }					
 ]	
